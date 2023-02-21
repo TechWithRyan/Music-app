4d31199defb8264c12b861ed90c7a8ea5e91ba66
RyanCornelio <ryan.cornelio@gmail.com>
2023-02-21 14:43:22 +0100

    mobile responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,8 +16,7 @@ const Home = () => {
 
         <section className='right'>
           <p>Greetings! My name is Ryan.</p><p>I'm an aspiring web developer. Feel free to read about me <a href="/about">here</a></p>
-          <p>I'm learning as I go & im really happy about this page so far.</p>
-          Questions? Ask me anything <a href="/Contact">here</a>
+         
         </section>
 
       </section>
