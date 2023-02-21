575ffcefad5ebf86dba4cb9a82212be9f62ab939
RyanCornelio <ryan.cornelio@gmail.com>
2023-02-21 16:34:59 +0100

    Bye bye typical

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-/* import Typical from 'react-typical' */
 import { Link } from 'react-router-dom'
 import ryan from "../assets/ryan.jpg"
 import "./Home.css"
@@ -16,29 +15,16 @@ const Home = () => {
         </section>
 
         <section className='right'>
-    {/* <Typical
-        steps={['<Hello world />', 5000, 'I`m an aspiring web developer 😊', 10000]}
-        loop={3}
-        wrapper="h1" className="typical-h1"
-      />
-    <Typical
-        steps={['<My name is Ryan />', 5000, 'Feel free to read about me...', 10000]}
-        loop={3}
-        wrapper="p" className="typical"
-      /> */}
-        
-
           <p>Greetings! My name is Ryan.</p><p>I'm an aspiring web developer. Feel free to read about me <Link to="/about">here</Link></p>
-         
         </section>
 
       </section>
-      <section className='home-info'>
+      {/* <section className='home-info'>
 
         <ul><li>Location: Ytterby, Kungälv Sweden.</li></ul>
         <ul><li>Age: 31</li></ul>
         <ul><li></li></ul>
-        </section> 
+      </section> */}
     </>
   )
 }
