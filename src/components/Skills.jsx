d8ee4b16122798969877269d96aeff7e90286e97
RyanCornelio <ryan.cornelio@gmail.com>
2023-02-24 10:26:24 +0100

    icons look good

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,6 +5,7 @@ import html5 from "../assets/html5.png"
 import JS from "../assets/JS.png"
 import node from "../assets/node.png"
 import react from "../assets/react.jpg"
+import WP from "../assets/WP.png"
 
 const Skills = () => {
   return (
@@ -15,11 +16,16 @@ const Skills = () => {
       <section className='icons-container'>
         <img src={css3} alt="" />
         <img src={html5} alt="" />
-        <img src={JS} alt="" className='js-icon'/>
+        <img src={JS} alt="" />
         <img src={node} alt="" />
         <img src={react} alt="" />
+        <img src={WP} alt="" />
       </section>
-      <section></section>
+      <section className='skills-info'>
+        <h1>Info</h1>
+        <p>I know HTML, CSS and some JS. I do find it easier to work with React. I have done some Wordpress in school as well. However I do focus mainly on React & I'm thinking 
+          to look into React native.</p>
+      </section>
     </>
   )
 }
