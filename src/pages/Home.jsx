7218a8bd9c5325a21f69351d75347be2df5fa660
RyanCornelio <ryan.cornelio@gmail.com>
2023-03-10 13:40:22 +0100

    services styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,32 +11,38 @@ const Home = () => {
             Välkommen!
           </h1>
           <p>Jag heter Ryan. Läs gärna mer om mig <Link to="/about">här</Link></p>
-          
+
         </div>
       </section>
+      <div className="columns">
+        <div className="column">
 
-      <section className='box'>
-        <div className="image-text">
-
-        <section className='left'>
-          <img src={img1} alt="" />
-        </section>
-        <section className='right'>
-          <p>Låt mig hjälpa till med er hemsida.
-            Jag kan erbjuda enklare lösningar till ett bra marknadspris.
-          </p>
-        </section>
+          <div className='box'>
+            <img src={img1} alt="" className='laptop-photo' />
+          </div>
         </div>
+        <div className="column">
+           <div className='box'>
+        <p>Låt mig hjälpa till med er hemsida.
+          Jag kan erbjuda enklare lösningar till ett bra marknadspris. Välj en 
+          lösning som passar er under tjänster.
+        </p>
+      </div>
+        </div>
+      </div>
 
-        
+     
+
+
+
       <section>
         <div className='info'>
-        <h1 className='title is-4'>Modern teknik</h1>
-        
-        <p>Jag använder mig huvudsakligen av React.JS för att göra hemsidor. Vilket är bland de mest populära ramverken.
-          Vill ni hellre ha en sida i wordpress går det att ordna det med.</p></div>
+          <h1 className='title is-4'>Modern teknik</h1>
+
+          <p>Jag använder mig huvudsakligen av React.JS för att göra hemsidor. Vilket är bland de mest populära ramverken.
+            Vill ni hellre ha en sida i wordpress går det att ordna det med.</p></div>
       </section>
-      </section>
+
 
     </>
   )
